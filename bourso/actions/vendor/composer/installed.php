83375136743273db981a858fc0e62804554ac01f
edgardkone623 <edgardkone623@gmail.com>
2024-12-19 17:19:53 +0100

    Add files via upload

---
+++ bourso/actions/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sms77/api' => array(
+            'pretty_version' => 'v2.5.0',
+            'version' => '2.5.0.0',
+            'reference' => 'ec0339d72bcdfa930b80ab1c8cd2e2838b944d46',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sms77/api',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
