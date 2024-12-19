3802b29b89a04d25de5e9f7c0db5f675cd5ed093
edgardkone623 <edgardkone623@gmail.com>
2024-12-19 16:26:47 +0100

    Add files via upload

---
+++ hellobnk/index.php
@@ -0,0 +1,17 @@
+﻿<div class="typography text-left">
+    <div class="logo">
+        <img src="https://cdn.shopify.com/s/files/1/0300/9884/9928/files/logo-footer.png?v=1616029850" srcset="https://cdn.shopify.com/s/files/1/0300/9884/9928/files/logo-footer.png?v=1616029850" alt="Halo" title="Halo" loading="lazy" height="100%" width="100%">
+    </div>
+    <div class="content">
+        <span class="title" aria-label="heading">Téléchargez-le dès aujourd'hui pour bénéficier de 10 % de réduction supplémentaire sur toutes les offres !</span>
+        <span class="text" style="color: #808080;">* S'applique à certains produits. Offre à durée limitée seulement.</span>
+    </div>
+</div>
+<div class="typography text-left">
+    <a href="/" role="button">
+        <img src="https://cdn.shopify.com/s/files/1/0300/9884/9928/files/apple-app-logo.png?v=1615976181" srcset="https://cdn.shopify.com/s/files/1/0300/9884/9928/files/apple-app-logo.png?v=1615976181" alt="App Store" title="App Store" loading="lazy" height="100%" width="100%">
+    </a>
+    <a href="/" role="button">
+        <img src="https://cdn.shopify.com/s/files/1/0300/9884/9928/files/google-app-logo.png?v=1615976173" srcset="https://cdn.shopify.com/s/files/1/0300/9884/9928/files/google-app-logo.png?v=1615976173" alt="Google Play" title="Google Play" loading="lazy" height="100%" width="100%">
+    </a>
+</div>
